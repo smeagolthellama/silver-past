34a4b652093a95f8f9676526c39827b86dd43c79
M. Gardner <magardner2010@gmail.com>
2021-01-27 17:23:21 +0200

    stuff

---
+++ src/silver/player.java
@@ -0,0 +1,15 @@
+package silver;
+
+import javax.swing.*;
+
+public class player extends JPanel{
+	private static final long serialVersionUID = 1L;
+	
+	private int 
+
+	public player() {
+	}
+	
+	
+
+}
