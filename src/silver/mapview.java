1980ff73bd54fd06d54d7bff6161d07102fde624
M. Gardner <magardner2010@gmail.com>
2021-01-27 13:40:24 +0200

    new stuff

---
+++ src/silver/mapview.java
@@ -0,0 +1,20 @@
+package silver;
+
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ * @author mark
+ *
+ */
+public class mapview {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
